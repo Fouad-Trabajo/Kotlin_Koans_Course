7db973934e7773e9e03a58a35a7f411ed7df092f
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-09-18 18:43:52 +0200

    Haciendo pruebas

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,2 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = customers.partition
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
