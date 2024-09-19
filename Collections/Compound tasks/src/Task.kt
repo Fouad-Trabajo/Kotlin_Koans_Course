f25158a6ade801f8d903bfe734450b9473d37bf4
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-09-19 20:20:19 +0200

    Finished collection exercises

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,14 +1,19 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
 fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
+    return customer.orders
+        .filter(Order::isDelivered)     // Filtra solo los pedidos entregado
+        .flatMap(Order::products)       // Obtén todos los productos de esos pedidos
+        .maxByOrNull(Product::price)    // Encuentra el producto más caro
 }
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+    return customers
+        .flatMap(Customer::getOrderedProducts)      // Obtener todos los productos que han pedido los clientes
+        .count { it == product }                    // Cuenta cuántas veces coincide con el producto
 }
 
 fun Customer.getOrderedProducts(): List<Product> =
-        /* TODO */
+    orders.flatMap(Order::products)
--- Collections/Sequences/src/Task.kt
+++ Collections/Sequences/src/Task.kt
@@ -1,14 +1,19 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
 fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
+    return customer.orders.asSequence() //Convertimos la lista de clientes en una secuencia
+        .filter(Order::isDelivered)
+        .flatMap(Order::products)
+        .maxByOrNull(Product::price)
 }
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+    return customers.asSequence() //Convertimos la lista de clientes en una secuencia
+        .flatMap(Customer::getOrderedProducts) //convertimos la lista de productos en una secuencia
+        .count { it == product }  // contamos cuántas veces coincide con el producto
 }
 
 fun Customer.getOrderedProducts(): Sequence<Product> =
-        TODO()
+    orders.asSequence().flatMap(Order::products) //Convertimos la lista de productos en una secuencia
